e17fd082059284ac4223e57c539447cb24e953aa
freemember007 <freemember007@gmail.com>
2015-08-10 12:07:47 +0800

    发消息数据

--- www/app/messages/messages.js
+++ www/app/messages/messages.js
@@ -2,15 +2,25 @@ angular.module('ddysys.controllers')
 
 
 //--------- 聊天controller ---------//
-.controller('MessagesCtrl', function($scope, Messages, $stateParams, $localStorage, _) {
+.controller('MessagesCtrl', function($scope, Messages, $stateParams, $localStorage, _, PostData, $http) {
+
+  $scope.user = $localStorage.getObject('user');
+  $scope.toUser = _.findWhere($localStorage.getObject('patients'), {patId: Number($stateParams.patientId)});
 
   Messages.all($stateParams.patientId).then(function(data){
     if(!data)return;
     $scope.messages = data.list;
   });
 
-  $scope.user = $localStorage.getObject('user');
-
-  $scope.toUser = _.findWhere($localStorage.getObject('patients'), {patId: Number($stateParams.patientId)});
+  $scope.sendMessage = function(){
+    var postData = new PostData('appsendmessage');
+    postData.patId = $stateParams.patientId;
+    postData.msgType = 'T';
+    postData.msgContent = '测试回复';
+    $http.post('api', postData).then(function(data){
+      if(!data) return;
+      alert('回复成功！')
+    })
+  }
 
 })
